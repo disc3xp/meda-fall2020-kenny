46fce98a429a74b177c9ab3ae09f7be92be2e5fe
Kenny <cqj15xp@gmail.com>
2020-09-30 13:09:43 -0700

    Looked at JavaScript 3 DataTypes and console.log() function

---
+++ assignments/week_5/firstjavascript.js
@@ -0,0 +1,56 @@
+// 9-30-2020:
+// Takes the text within the quotes and sends it to the Javescript Console.
+console.log("Hello world!");
+
+console.log("How are you doing?");
+
+console.log("My name is Kenny.");
+
+// This is a single line comment
+
+/*
+multi-line
+comment 
+*/
+
+// JavaScript DATATYPES:
+// String  (means normal text, a collection of characters, **use double quote)
+"This is a really long sentence. !$#^%$#^%$#&^%";
+"To use single quotes ' and ' in the string, use double quotes on the outside.";
+'To use double quotes " and " in the string, use single quotes on the outside.';
+"100";
+
+// Number (******Dont need quotation marks around the numbers, for javascript)
+100;
+10.9;
+.70009;
+
+// Boolean (means a switch)
+true;
+false;
+
+
+/* 
+The computer will break on the "second" string
+For example, this is wrong, will break the code, and will give error: 
+" Hellow this is a double quote" "Its used for text"; Incorrect
+' Hellow this is a double quote" "Its used for text'; Correct
+*/
+
+// 'There's a nice day!' Incorrect
+// "There's a nice day!" Correct
+
+
+console.log(1000);
+console.log(true);
+console.log("Hi there");
+
+
+
+/*
+Just for exmaple, how fast the computer can count; counting from 1 to 100000
+
+for (var i = 0; i < 100000; i++) (
+    console.log(i);
+)
+*/
